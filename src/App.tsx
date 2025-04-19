a3bf5a7f7296685509a365013fbc8f1b0f07d7a1
lebrancconvas <lebrancconvas@gmail.com>
2025-04-19 14:59:56 +0700

    ✨ feat: draft the front-end structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 function App() {
   return (
     <>
-    
+      <header>
+        <h1>Save State Demo for Visual Novel</h1>
+      </header>
+      <section id="content">
+        Content of Visual Novel
+      </section>
+      <section id="control">
+        <section id="save">
+          <button>Save</button>
+        </section>
+        <section id="load">
+          <button>Load</button>
+        </section>
+      </section>
+      <footer>
+        <p>Save State Demo for Visual Novel</p>
+        <p>Version 0.1.0</p>
+        <p>Author: Poom Yimyuean (@lebrancconvas)</p>
+      </footer>
     </>
   )
 }
